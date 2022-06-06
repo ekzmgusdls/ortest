9bbc97318c19697fd8b59d994e3a30c518eeba01
Hwanghyunbeom <ekzmgusdls@gmail.com>
2022-06-06 23:26:48 +0900

    Some

--- orientation.js
+++ orientation.js
@@ -1,14 +1,14 @@
-function handleOrientation(event) {
-    var absolute = event.absolute;
-    var alpha = event.alpha;
-    var beta = event.beta;
-    var gamma = event.gamma;
-
-    document.getElementById('test').innerText = `absolute: ${absolute}, alpha: ${alpha}, beta: ${beta}, amma: ${gamma}`;
-    // Do stuff with the new orientation data
-}
-
 window.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener('deviceorientation', handleOrientation, true);
+    function handleOrientation(event) {
+        var absolute = event.absolute;
+        var alpha = event.alpha;
+        var beta = event.beta;
+        var gamma = event.gamma;
+
+        document.getElementById('test').innerText = `absolute: ${absolute}, alpha: ${alpha}, beta: ${beta}, amma: ${gamma}`;
+        // Do stuff with the new orientation data
+    }
+
     document.getElementById('do').innerText = 'Now this is doing';
+    window.addEventListener('deviceorientation', handleOrientation, true);
 });
